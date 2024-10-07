7dce1571adcc3527146e9c6695bc3c382e8cbdce
ruizlo04 <carlo@CalosRuizLopez>
2024-10-07 19:41:03 +0200

    test finalizados

--- Sistema de gestión empresarial/Ufos/Ufos/src/ufos.py
+++ Sistema de gestión empresarial/Ufos/Ufos/src/ufos.py
@@ -6,10 +6,9 @@ from math import sqrt
 from collections import namedtuple
 
 Avistamiento = namedtuple('Avistamiento', 'fechahora, ciudad, estado, forma, duracion, comentarios, latitud, longitud')
-# Test de la función lee_avistamientos
 def lee_avistamientos(fichero):
     res= []
-   
+
     with open(fichero, encoding='utf-8') as f:
         lector = csv.reader(f)
         next(lector)
@@ -26,24 +25,53 @@ def lee_avistamientos(fichero):
             res.append(Avistamiento(fechahora, ciudad, estado, forma, duracion, comentarios, latitud, longitud))
 
     return res  
+
 #Test de la función duracion_total
 def duracion_total(avistamientos, estado):
     res = 0
     for r in avistamientos:
         if r.estado == estado:
-            res += r.duracion
+            res += float(r.duracion)
     return res
 
     
 # Test de la función comentario_mas_largo
-
+def comentario_mas_largo(avistamientos):
+    res = ''
+    maximo = 0
+    for r in avistamientos:
+        if len(r.comentarios) > maximo:
+            maximo = len(r.comentarios)
+            res = r.comentarios
+    return res
 
 
 # Test de la función avistamientos_fechas
-
+def avistamientos_fechas(avistamientos, inicio, fin):
+    res = []
+    for r in avistamientos:
+        if r.fechahora >= inicio and r.fechahora <= fin:
+            res.append(r)
+    return res
 
 
 # Test de la función hora_mas_avistamientos
+def hora_mas_avistamientos(avistamientos):
+    res = {}
+    for r in avistamientos:
+        if r.fechahora.hour in res:
+            res[r.fechahora.hour] += 1
+        else:
+            res[r.fechahora.hour] = 1
+    maximo = 0
+    hora = 0
+    for k, v in res.items():
+        if v > maximo:
+            maximo = v
+            hora = k
+    return hora
+
+
 
 
 
--- Sistema de gestión empresarial/Ufos/Ufos/src/ufos_test.py
+++ Sistema de gestión empresarial/Ufos/Ufos/src/ufos_test.py
@@ -1,6 +1,16 @@
 from ufos import *
 
 
-avistamientos = lee_avistamientos('Ufos\data\ovnis.csv')
+avistamientos = lee_avistamientos('data/ovnis.csv')
 for a in avistamientos:
     print(a)
+
+print(duracion_total(avistamientos, 'CA'))  
+
+print(comentario_mas_largo(avistamientos))
+
+inicio = datetime.strptime('01/01/2000 00:00', '%m/%d/%Y %H:%M')
+fin = datetime.strptime('01/01/2001 00:00', '%m/%d/%Y %H:%M')
+print(avistamientos_fechas(avistamientos, inicio, fin))
+
+print(hora_mas_avistamientos(avistamientos))
